be12c521d2171da80cb3e7337a884849ff7e5c9e
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2023-12-19 19:00:26 +0600

    New update to Contact page and submit to user info then automatic Save to database this is user data .

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    subject = models.CharField(max_length=150)
+    massage = models.TextField(max_length=500)
--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
 def home(request):
     context={"home":"active"}
     return render(request,'core/home.html',context)
 
 def contact(request):
+    if request.method == "POST":
+        nm = request.POST.get('name')
+        em = request.POST.get('email')
+        sub = request.POST.get('subject')
+        txt = request.POST.get('message')
+        enqury = User(name=nm, email=em, subject=sub, massage=txt)
+        enqury.save()
     context={"contact":"active"}
     return render(request,'core/contact.html',context)
